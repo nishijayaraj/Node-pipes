e0d16105d3075b2987833e7a585886f8060f77c4
nishijayaraj <nishijayaraj@gmail.com>
2017-10-16 18:35:38 +0530

    Feature : Added finish event listener

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const fs = require("fs");
 const stream = fs.createWriteStream("demo.txt");
 stream.write("First line to the file\n");
 stream.write("Now it is the second line\n");
+/**
+ * finish event is triggered on closing the stream with end(),
+ *  without which finish event would never be triggered*/
+
+
+stream.on('finish',() => console.log("File has been written"));
+stream.end();
 
 
