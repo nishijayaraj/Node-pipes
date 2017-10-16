ae6af5b6f8a2ee938ba4bc22e102016800a14cf1
nishijayaraj <nishijayaraj@gmail.com>
2017-10-16 17:02:24 +0530

    Fix : Lines will be added in new line

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const stream = fs.createWriteStream("demo.txt");
-stream.write("First line to the file");
-stream.write("Now it is the second line");
+stream.write("First line to the file\n");
+stream.write("Now it is the second line\n");
 
 
